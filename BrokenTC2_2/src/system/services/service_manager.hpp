74d7280c2e4e4157926c050f353254fccbb1d93a
BrokenGameNoob <100915886+BrokenGameNoob@users.noreply.github.com>
2024-10-06 23:30:20 +0200

    WIP - not even compiling

--- BrokenTC2_2/src/system/services/service_manager.hpp
+++ BrokenTC2_2/src/system/services/service_manager.hpp
@@ -7,6 +7,7 @@
 #include <DataStructures/structures.hpp>
 #include <Logger/btype.hpp>
 #include <QSDL/game_controller.hpp>
+#include <WinUtils/window_change_hook.hpp>
 #include <games/gear_handler_factory.hpp>
 #include <games/gear_handler_the_crew.hpp>
 #include <system/services/controller_handler.hpp>
@@ -29,9 +30,13 @@ class ServiceManager : public QObject {
   Q_PROPERTY(double sdlAxisThreshold READ GetSDLAxisThreshold WRITE UpdateSDLAxisThreshold NOTIFY
                  sdlAxisThresholdModified FINAL)
 
+  Q_PROPERTY(QString focusedWindowTitle READ GetFocusedWindowTitle NOTIFY focusedWindowTitleChanged FINAL)
+  // Q_PROPERTY(Game:: name READ name WRITE setName NOTIFY nameChanged FINAL)
+
  signals:
   void gearHandlerChanged();
   void sdlAxisThresholdModified();
+  void focusedWindowTitleChanged();
 
  public:
   ~ServiceManager();
@@ -86,6 +91,14 @@ class ServiceManager : public QObject {
     return m_sdl_axis_threshold;
   }
 
+  /* Windows related */
+  static void CALLBACK OnWindowChangeHook(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idObject, LONG idChild,
+                                          DWORD dwEventThread, DWORD dwmsEventTime);
+  void OnFocusedWindowChanged(const QString& title);
+  QString GetFocusedWindowTitle() const {
+    return m_focused_window_title;
+  }
+
   /* Main */
   Q_INVOKABLE void OnMainWindowLoaded();
 
@@ -100,6 +113,9 @@ class ServiceManager : public QObject {
   std::unique_ptr<BaseGearHandler> m_gear_handler{nullptr};
 
   std::unique_ptr<KeyboardProfile> m_keyboard_profile{nullptr};
+
+  win::WinHookOwner m_window_change_hook;
+  QString m_focused_window_title;
 
   double m_sdl_axis_threshold{0.5};
 };
